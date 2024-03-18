344b7ce5bf3cfef3f29ca1b937763056246f5445
aditpgnSkom <littlecommander46@gmail.com>
2024-03-18 10:17:27 +0700

    feat - decode URL

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -4,15 +4,16 @@ import Header from "@/components/MangaList/Header";
 
 export default async function page({ params }) {
   const { keyword } = params;
+  const decodeKeyword = decodeURI(keyword);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/manga?q=${keyword}`
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/manga?q=${decodeKeyword}`
   );
   const searchManga = await response.json();
 
   return (
     <>
       <section>
-        <Header title={`Pencarian untuk ${keyword}...`} />
+        <Header title={`Pencarian untuk ${decodeKeyword}...`} />
         <MangaListComponents data={searchManga} />
       </section>
     </>
