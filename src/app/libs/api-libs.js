a9b5cbe00c09b0fdc6d0e7f14c943ccd73583641
aditpgnSkom <littlecommander46@gmail.com>
2024-03-18 12:59:05 +0700

    init - api libs

---
+++ src/app/libs/api-libs.js
@@ -0,0 +1,7 @@
+export const getMangaResponse = async ({ resource, query }) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
+  );
+  const data = await response.json();
+  return data;
+};
